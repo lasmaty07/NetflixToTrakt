39a62ae0e97a8cb2093e12bd721074c41df9c5fd
Matt <matias.brigante@gmail.com>
2021-07-18 19:28:15 -0300

    merge 2 search functions

--- SyncHistoryTracktv.py
+++ SyncHistoryTracktv.py
@@ -63,68 +63,61 @@ def api_auth():
         _headers['trakt-api-key'] = os.getenv("TRATK_API_KEY")
         print('Save as "oauth_token" in file {0}: {1}'.format(envars, response["access_token"]))
 
-def search_movies():
-  global _final_request,_duplicates
+def search():
+  """Search movies or tv shows and find it's trakt id"""
+  global _final_request
   m = {}
-  for movie_to_search in _items:
-    movie_title = movie_to_search.replace(' ','%20')
-    response = requests.get(_baseurl + '/search/movie?query='+movie_title, headers=_headers)
+  for item_to_search in _items:
+
+    if item_to_search.find(':')>0:
+      pos1 = item_to_search.find(':')
+      pos2 = item_to_search[item_to_search.find(':')+2:].find(':')
+      show_title = item_to_search[:pos1]
+      episode_name = item_to_search[pos1+pos2+2+2:]
+
+      title = episode_name
+      response = requests.get(_baseurl + '/search/episode?query='+title.replace(' ','%20'), headers=_headers)
+      type = 'episode'
+      type2 = 'episodes'
+    else:
+      title = item_to_search
+      response = requests.get(_baseurl + '/search/movie?query='+title.replace(' ','%20'), headers=_headers)
+      type = 'movie'
+      type2 = 'movies'
+      
     if response:
       json_data = json.loads(response.text)
       i = 0
-      for movie in json_data:            
-        if movie['movie']['title'] == movie_to_search:
+      for item_found in json_data:            
+        if item_found[type]['title'] == title and (type == 'movie' or item_found['show']['title'] == show_title):
           i += 1 
-          watched_at = datetime.datetime.strptime(_items[movie_to_search], '%m/%d/%y')
+          watched_at = datetime.datetime.strptime(_items[item_to_search], '%m/%d/%y')
           # time un 2020-09-12T00:00:00.000Z 
           m = {"watched_at": watched_at.strftime("%Y-%m-%dT%H:%M:%S.000Z"),
-          "title": movie['movie']['title'],
+          "title": item_found[type]['title'],
           "ids": {
-            "trakt": movie['movie']['ids']['trakt'],
-            "imdb": movie['movie']['ids']['imdb']
+            "trakt": item_found[type]['ids']['trakt'],
+            "imdb": item_found[type]['ids']['imdb']
             }
           }
           if i >1:
-            _duplicates['movies'].append(movie['movie']['title'] + ' ' + str(movie['movie']['year']) + ' Imdb id: '+ (movie['movie']['ids']['imdb'] if movie['movie']['ids']['imdb'] else 'null') + ' https://trakt.tv/movies/'+ str(movie['movie']['ids']['trakt']))
+            addDuplicate(item_found,item_to_search,type,type2)
       else:
         if m !={} and i==1:
           _movies_matched.append(m)
   if _movies_matched:          
-    _final_request['movies'] = _movies_matched
+    _final_request[type2] = _movies_matched
 
-def search_shows():
-  global _final_request,_duplicates
-  e = {}
-  for show_to_search in _items:
-    pos1 = show_to_search.find(':')
-    pos2 = show_to_search[show_to_search.find(':')+2:].find(':')
-    show_title = show_to_search[:pos1]
-    episode_name = show_to_search[pos1+pos2+2+2:]
+def addDuplicate(item_found,item_to_search,type,type2):
+  global _duplicates
 
-    title = episode_name.replace(' ','%20')
-    response = requests.get(_baseurl + '/search/episode?query='+title, headers=_headers)
-    if response:
-      json_data = json.loads(response.text)
-      i = 0
-      for episode in json_data:            
-        if episode['episode']['title'] == episode_name and episode['show']['title'] == show_title:
-          i += 1 
-          watched_at = datetime.datetime.strptime(_items[show_to_search], '%m/%d/%y')
-          # time un 2020-09-12T00:00:00.000Z 
-          e = {
-              "watched_at": watched_at.strftime("%Y-%m-%dT%H:%M:%S.000Z"),
-              "ids": {
-                  "trakt": episode['episode']['ids']['trakt'],
-                  "imdb": episode['episode']['ids']['imdb']
-              }
-          }
-          if i >1:
-            _duplicates['movies'].append(show_to_search)
-      else:
-        if e !={} and i==1:
-          _shows_matched.append(e)
-  if _shows_matched:
-    _final_request['episodes'] = _shows_matched
+  item = {"title":item_found[type]['title'],
+          "year" : str(item_found[type]['year']),
+          "Imdb_id": (item_found[type]['ids']['imdb'] if item_found[type]['ids']['imdb'] else 'null'),
+          "trakt":item_found[type]['ids']['trakt'],
+          "URL": ' https://trakt.tv/movies/'+ str(item_found[type]['ids']['trakt'])
+    }
+  _duplicates[type2].append(item)
 
 def main():
   #load from csv
@@ -137,19 +130,20 @@ def main():
   print(_headers)
 
   print('Found ' + str(len(_items)) + ' items to import\n')
-  search_movies()
-  search_shows()
-  print('\nFound duplicates\n')
+  #search_movies()
+  #search_shows()
+  search()
+  print('\n--------------------Found duplicates--------------------\n')
   print(_duplicates)
-  print('\nFinal Request\n')
-  print(_final_request)
+  print('\n--------------------Final Request--------------------\n')
+  print(json.dumps(_final_request))
   
   with open('duplicates.json', 'w') as f:
     print(json.dumps(_duplicates), file=f)
 
   # invoke   
-  response =requests.post( _baseurl + '/sync/history',data=json.dumps(_final_request), headers=_headers)
-  print(response)
+  #response =requests.post( _baseurl + '/sync/history',data=json.dumps(_final_request), headers=_headers)
+  #print(response)
 
   sys.exit(0)
 

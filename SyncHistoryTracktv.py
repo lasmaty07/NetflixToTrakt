de38b717ed61a4fec42f85caed19fc16f98ef974
Matt <matias.brigante@gmail.com>
2021-07-21 22:50:28 -0300

    add search to class

--- SyncHistoryTracktv.py
+++ SyncHistoryTracktv.py
@@ -48,6 +48,55 @@ class NetflixItems():
     except IOError as e:
       logging.error(f'failed to load: {fileName}',e)
 
+  def search_items(self):
+    """Search movies or tv shows and find it's trakt id"""
+    global _final_request
+    m = {}
+    for item_to_search in self.items:
+
+      if item_to_search.find(':')>0:
+        pos1 = item_to_search.find(':')
+        pos2 = item_to_search[item_to_search.find(':')+2:].find(':')
+        show_title = item_to_search[:pos1]
+        episode_name = item_to_search[pos1+pos2+2+2:]
+
+        title = episode_name
+        response = requests.get(_baseurl + '/search/episode?query='+title.replace(' ','%20'), headers=_headers)
+        type = 'episode'
+        type2 = 'episodes'
+      else:
+        title = item_to_search
+        response = requests.get(_baseurl + '/search/movie?query='+title.replace(' ','%20'), headers=_headers)
+        type = 'movie'
+        type2 = 'movies'
+        
+      if response:
+        json_data = json.loads(response.text)
+        i = 0
+        item_found_prev = {type:{}}
+        for item_found in json_data:
+
+          if item_found[type]['title'].lower() == title.lower() and (type == 'movie' or item_found['show']['title'] == show_title):
+            
+            watched_at = datetime.datetime.strptime(self.items[item_to_search], '%m/%d/%y')
+            # time un 2020-09-12T00:00:00.000Z 
+            m = {"watched_at": watched_at.strftime("%Y-%m-%dT%H:%M:%S.000Z"),
+            "title": item_found[type]['title'],
+            "ids": {
+              "trakt": item_found[type]['ids']['trakt'],
+              "imdb": item_found[type]['ids']['imdb']
+              }
+            }
+
+            if i!=0 and item_found[type]['title'].lower() == item_found_prev[type]['title'].lower():
+              addDuplicate(item_found,item_to_search,type,type2,watched_at)
+
+            item_found_prev = item_found
+            i = i + 1 
+        else:
+          if m !={} and i==1:
+            _final_request[type2].append(m)      
+
 def api_auth():
   """API call for authentification OAUTH"""
   if not(os.getenv("TOKEN")):  
@@ -70,55 +119,6 @@ def api_auth():
   _headers['Authorization'] = 'Bearer ' + response["access_token"]
   _headers['trakt-api-key'] = os.getenv("TRATK_API_KEY")
   print('Save as "oauth_token" in file {0}: {1}'.format(envars, response["access_token"]))
-
-def search():
-  """Search movies or tv shows and find it's trakt id"""
-  global _final_request
-  m = {}
-  for item_to_search in _items:
-
-    if item_to_search.find(':')>0:
-      pos1 = item_to_search.find(':')
-      pos2 = item_to_search[item_to_search.find(':')+2:].find(':')
-      show_title = item_to_search[:pos1]
-      episode_name = item_to_search[pos1+pos2+2+2:]
-
-      title = episode_name
-      response = requests.get(_baseurl + '/search/episode?query='+title.replace(' ','%20'), headers=_headers)
-      type = 'episode'
-      type2 = 'episodes'
-    else:
-      title = item_to_search
-      response = requests.get(_baseurl + '/search/movie?query='+title.replace(' ','%20'), headers=_headers)
-      type = 'movie'
-      type2 = 'movies'
-      
-    if response:
-      json_data = json.loads(response.text)
-      i = 0
-      item_found_prev = {type:{}}
-      for item_found in json_data:
-
-        if item_found[type]['title'].lower() == title.lower() and (type == 'movie' or item_found['show']['title'] == show_title):
-          
-          watched_at = datetime.datetime.strptime(_items[item_to_search], '%m/%d/%y')
-          # time un 2020-09-12T00:00:00.000Z 
-          m = {"watched_at": watched_at.strftime("%Y-%m-%dT%H:%M:%S.000Z"),
-          "title": item_found[type]['title'],
-          "ids": {
-            "trakt": item_found[type]['ids']['trakt'],
-            "imdb": item_found[type]['ids']['imdb']
-            }
-          }
-
-          if i!=0 and item_found[type]['title'].lower() == item_found_prev[type]['title'].lower():
-            addDuplicate(item_found,item_to_search,type,type2,watched_at)
-
-          item_found_prev = item_found
-          i = i + 1 
-      else:
-        if m !={} and i==1:
-          _final_request[type2].append(m)
 
 def addDuplicate(item_found,item_to_search,type,type2,watched_at):
   global _duplicates
@@ -158,18 +158,16 @@ def importValidatedDuplicates(file):
     logging.error(f'cannot open file {file}',e)
 
 def main():
-  #load from csv
-  #global _items
+
   items = NetflixItems()
   items.read_csv(csvFile)
-
   
   api_auth()
 
-  print(_headers)
-
   print('Found ' + str(len(items.items)) + ' items to import\n')
-  search()
+
+  items.search_items()
+
   if len(_duplicates['movies'])>0:
     print('\nFound duplicate movies check duplicates.json\n')
 
@@ -177,6 +175,7 @@ def main():
     print('\nFound duplicate episodes check duplicates.json\n')    
 
   importValidatedDuplicates('duplicatesValidated.json')
+  
   print('\n--------------------Final Request--------------------\n')
   print(json.dumps(_final_request))
   

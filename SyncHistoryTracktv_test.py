c253f0dd47a9888984749d8a82d098332f34b076
Matt <matias.brigante@gmail.com>
2021-10-02 13:26:18 -0300

    add class show and format black

--- SyncHistoryTracktv_test.py
+++ SyncHistoryTracktv_test.py
@@ -1,13 +1,40 @@
 import unittest
 
 import SyncHistoryTracktv as sh
+from movie import Movie
+from show import Show
 
 
 class TestSum(unittest.TestCase):
-  """def test_list_int(self):
+    """def test_list_int(self):
     data = [1, 2, 3]
     result = sum(data)
     self.assertEqual(result, 6)"""
 
-if __name__ == '__main__':
+    pass
+
+
+class TestMovie(unittest.TestCase):
+    """
+    Test movie class
+    """
+
+    def testValidMovie(self):
+        title = "titulo 1"
+        movie = Movie(title)
+        self.assertEqual(movie.title, title, f"This should be {title}")
+        self.assertEqual(movie.type, "movie", f"This should be movie")
+        self.assertEqual(movie.type2, "movies", f"This should be movie")
+
+    def testValidShow(self):
+        show_name = "this is a show title"
+        episode_name = "This is a show name"
+        show = Show(show_name, episode_name)
+        self.assertEqual(show.episode_name, episode_name, f"this should be {episode_name}")
+        self.assertEqual(show.show_name, show_name, f"this should be {show_name}")
+        self.assertEqual(show.type, "episode", "this should be Episode")
+        self.assertEqual(show.type2, "episodes", "this should be Episodes")
+
+
+if __name__ == "__main__":
     unittest.main()
